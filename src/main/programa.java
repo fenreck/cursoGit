d3db99b43915a6d4c5d04a9bf9807e35ff7ef3b3
fenreck <fenreck@hotmail.com>
2021-10-13 11:52:49 -0500

    Archivos iniciales

---
+++ src/main/programa.java
@@ -0,0 +1,43 @@
+package main;
+
+public class programa {
+	static  int sumaTotal = 0;
+	public static void main(String[] args) {
+		
+			int [] arr = {-1,-1,-1};
+			
+		System.out.println(  sum(arr) );
+		
+
+	}
+	
+	public static int sum(int[] arr) {
+		int contador = 0;
+		return sum2 (arr, contador);
+		
+	}
+	
+	public static int sum2 ( int []  arr, int con ) {
+	
+
+		
+		if (arr.length == 0) {
+			return 0;
+		}
+		
+		if (con < arr.length ) {
+	
+			sumaTotal += arr [con++];
+		//	System.out.println(  sumaTotal );
+			sum2(arr, con);
+			
+		
+		}
+		
+		
+		
+		return sumaTotal;
+	}
+	
+
+}
